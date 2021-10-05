3d60d946a09812850ecc37d1dc79912e4d9b9f03
hsien201412 <hsien201412@gmail.com>
2021-10-05 16:38:31 +0800

    修正php_constants1.php

--- ch03/php_constants1.php
+++ ch03/php_constants1.php
@@ -2,7 +2,7 @@
 	define('eHappy',"文淵閣工作室");
 	const eHappyUrl = "http://www.e-happy.com.tw";
 	echo "您好，歡迎光臨".eHappy."的網站<br>";
-	echo "網址為：<a href='>".eHappyUrl."'>文淵閣工作室</a>";
+	echo "網址為：<a href='".eHappyUrl."'>文淵閣工作室</a>";
 	/*超連結的html
 	語法:<a href='連結網址'>連結名稱</a>
 	<a href='a.php'>a網頁</a>
