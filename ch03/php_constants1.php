921fc776a411cd7f35e160e2854aba39f8577802
hsien201412 <hsien201412@gmail.com>
2021-10-05 16:30:21 +0800

    增加超連結語法html+php

--- ch03/php_constants1.php
+++ ch03/php_constants1.php
@@ -2,5 +2,15 @@
 	define('eHappy',"文淵閣工作室");
 	const eHappyUrl = "http://www.e-happy.com.tw";
 	echo "您好，歡迎光臨".eHappy."的網站<br>";
-	echo "網址為：".eHappyUrl;
+	echo "網址為：<a href='>".eHappyUrl."'>文淵閣工作室</a>";
+	/*超連結的html
+	語法:<a href='連結網址'>連結名稱</a>
+	<a href='a.php'>a網頁</a>
+	<a href='http://www.slime.com.tw'>史萊姆第一個家</a>
+	echo "網址為：<a href='>".eHappyUrl."'>文淵閣工作室</a>";
+	拆解為:
+	1.網址為:<a href='
+	2.eHappyUrl-->常數
+	3.'>文淵閣工作室</a>
+	*/
 ?>
