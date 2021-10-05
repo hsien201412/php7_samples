f27954921e2a16d9abb75769f846645169c56f47
hsien201412 <hsien201412@gmail.com>
2021-10-05 16:39:33 +0800

    修改

--- ch03/php_constants1.php
+++ ch03/php_constants1.php
@@ -7,7 +7,7 @@
 	語法:<a href='連結網址'>連結名稱</a>
 	<a href='a.php'>a網頁</a>
 	<a href='http://www.slime.com.tw'>史萊姆第一個家</a>
-	echo "網址為：<a href='>".eHappyUrl."'>文淵閣工作室</a>";
+	echo "網址為：<a href='".eHappyUrl."'>文淵閣工作室</a>";
 	拆解為:
 	1.網址為:<a href='
 	2.eHappyUrl-->常數
