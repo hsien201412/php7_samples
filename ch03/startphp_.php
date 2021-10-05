894f98903e5346c5639554affd4fcd5b308905cd
hsien201412 <hsien201412@gmail.com>
2021-10-05 15:03:07 +0800

    startphp_.php程式碼增加註解

--- ch03/startphp_.php
+++ ch03/startphp_.php
@@ -1,11 +1,13 @@
 <html>
 <head>
-<title>�ڪ��Ĥ@��PHP�{��</title>
+<title>我不是亂碼啦!!!</title>
 </head>
 <body>
-
+	<!-- html程式註解 -->
 <?php 
-	echo "�١APHP�A�n!!"; 
+	echo "我不是亂碼啦!!!"; 
+	//單行註解
+	/*多行文字註解 */
 ?>
 
 </body>
